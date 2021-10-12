acfded6617f913d8754ce31281a183845c6f3d78
Haneen Hashlamoun <haneen.hashlamoun@gmail.com>
2021-10-12 21:16:55 +0300

    array-insert-shift code challenge 2

--- python/code_challenges/array-insert-shift/array-insert-shift.py
+++ python/code_challenges/array-insert-shift/array-insert-shift.py
@@ -3,8 +3,6 @@ value2=12
 # list1=[2,4,6,-8]
 list2=[42,8,15,23,42]
 def insertShiftArray(listParam,value1):
-  if not( type(listParam) == type([]) ):
-    return 'ERROR'
   myList=[]
   if len(listParam)%2==0:
     result=int(len(listParam)/2)
